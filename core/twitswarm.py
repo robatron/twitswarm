a79b22b2d0512dfa4609dc0d46cf410e2042d8e6
Rob McGuire-Dale <rob@osuosl.org>
2011-01-28 22:08:45 -0800

    Adding base codefile and the all-important applicaiton title printing.

---
+++ core/twitswarm.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+'''
+T W I T S W A R M
+
+This is the main codefile for twitswarm.
+'''
+
+def main():
+    ''' Run at program start '''
+    print '-------------------'
+    print ' T W I T S W A R M '
+    print '-------------------'
+    exit(0)
+
+# run main once everything's loaded
+if __name__ == '__main__':
+    main()
