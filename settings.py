029b3484e952abb221e08dc1d8363c87cb34db25
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2011-02-11 20:27:57 -0800

    Adding temp settings file.

---
+++ settings.py
@@ -0,0 +1,11 @@
+# Settings for this twitswarm agent
+
+# Swarm ID - The ID of the swarm. Used to address the entire swarm. Implemented
+# as standard twitter hashtags 
+# (http://twitter.pbworks.com/w/page/1779812/Hashtags)
+SWARM_ID = "MySwarm"
+
+# Swarm secret key - The swarm's shared key. Used for inter-agent 
+# authentication.
+SWARM_SECRET_KEY = "Super secret! (Said with a lisp)"
+
