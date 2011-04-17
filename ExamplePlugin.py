7dc52d0428fd66864c715ffd8131064befb285aa
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2011-04-16 19:05:44 -0700

    Moving to the underscore space convention.

--- ExamplePlugin.py
+++ ExamplePlugin.py
@@ -1,25 +1,22 @@
 '''
-Example plugin
-
-Just messing around. No real functionality yet! Attempting to use all of the
-API functions.
+An example plugin that listens for "Hello world!" and responds to the sender 
+with "'sup".
 '''
 
-from twitAPI import feeds, listen
+from twit_plugin import base_plugin, feeds
 
-class ExamplePlugin():
+class plugin__hello_world(base_plugin):
     def __init__(self):
         '''Initialize this example plugin.'''
 
-        self.listen = listen()
-
+        # register a listener for 'hello world'
         self.listen.listen(
             r'Hello world!',            # regex of what to listen for
             feeds.ALL,                  # what feed to listen on
-            self.helloWorldResponder    # function to call when it matches 
+            self.hello_world_responder  # function to call when it matches 
         )
 
-    def helloWorldResponder(self, tweet):
+    def hello_world_responder(self, tweet):
         ''' Responder for "Hello world!" tweets '''
         print tweet
 
