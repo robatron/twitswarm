5c36d01bd2a1a9a2fe5d01e270f4a8733d40bef8
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2011-04-18 12:45:06 -0700

    Moving the body of twitswarm into its own class to encapsulate data and functions better.

--- twitswarm.py
+++ twitswarm.py
@@ -4,6 +4,7 @@ A distributed twitter bot platform.
 '''
 
 import time
+import json
 from twitter import oauth_dance, OAuth, Twitter
 from twitter import TwitterHTTPError
 
@@ -13,68 +14,75 @@ import app_info
 # temporary plugin imports (will eventually be handled by the plugin manager)
 from plugin__hello_world import hello_world
 
-plugins = []
-leTwitter = None
-
 def main():
     ''' Runs at program start '''
-    print '-------------------'
-    print ' T W I T S W A R M '
-    print '-------------------'
-    print 'Hello from Agent %s!'%TWITTER_USERNAME
-    load_plugins()
-    authenticate()
-    start()
+    ts = twitswarm()
+    ts.start()
     exit(0)
 
-def authenticate():
-    print "Authenticating with twitswarm. Follow directions in browser."
-    print "--"
-    try:
-        token_key, token_secret = oauth_dance.oauth_dance(app_info.APP_NAME, 
-                app_info.CONSUMER_KEY, app_info.CONSUMER_SECRET)
-        oauth = OAuth(token_key, token_secret, app_info.CONSUMER_KEY, 
-                app_info.CONSUMER_SECRET)
-        leTwitter = Twitter(auth=oauth)
-    except TwitterHTTPError:
-        print "Authentication error!"
-        byebye(1)
-    except KeyboardInterrupt:
-        byebye()
-    print "--"
-    print "Authenticated!"
+class twitswarm:
+    plugins = []
+    twitter = None
+    test_val = None   
 
-def load_plugins():
-    plugins.append(hello_world())
+    def __init__(self):
+        print '--[twitswarm]----------'
+        print 'Hello from Agent %s!'%TWITTER_USERNAME
+        self.load_plugins()
+        self.authenticate()
 
-def start():
-    ''' Main program loop. '''
-    print 'Starting main program loop. Press Ctrl+c to exit.'
+    def authenticate(self):
+        print "Starting authentication dance:"
+        print "--"
 
-    while True:
         try:
-            for plugin in plugins:
-
-                # listen for each plugin's registered listeners
-                for listener_spec in plugin.listeners:
-                    listen_for(listener_spec['search_query'], 
-                            listener_spec['handler'])
-
-                # sleep for a bit to respect the twitter API's rate limiting
-                time.sleep(TWITTER_POLL_INTERVAL)
-        # quit on Ctrl+c
+            token_key, token_secret = oauth_dance.oauth_dance(
+                    app_info.APP_NAME, app_info.CONSUMER_KEY, 
+                    app_info.CONSUMER_SECRET)
+            oauth = OAuth(token_key, token_secret, app_info.CONSUMER_KEY, 
+                    app_info.CONSUMER_SECRET)
+            self.twitter = Twitter(auth=oauth)
+        except TwitterHTTPError:
+            print "Authentication error!"
+            self.finish(1)
         except KeyboardInterrupt:
-            byebye()
+            self.finish()
 
-def listen_for(query, handler):
-    print 'Performing search on ``%s``, calling ``%s`` on matched results'\
-            %(query,handler.__name__)
-    print leTwitter.search(q=query)
+        print "--"
+        print "Authenticated!"
+
+    def load_plugins(self):
+        self.plugins.append(hello_world())
+
+    def start(self):
+        ''' Main program loop. '''
+        print 'Starting main program loop. Press Ctrl+c to exit.'
+
+        while True:
+            try:
+                for plugin in self.plugins:
+
+                    # listen for each plugin's registered listeners
+                    for listener_spec in plugin.listeners:
+                        self.look_for(listener_spec['search_query'], 
+                                listener_spec['handler'])
+
+                    # sleep for a bit as to not anger le twitters
+                    time.sleep(TWITTER_POLL_INTERVAL)
+
+            # quit on Ctrl+c
+            except KeyboardInterrupt:
+                self.finish()
+
+    def look_for(self, query, handler):
+        print 'Performing search on ``%s``, calling ``%s`` on matched results'\
+                %(query,handler.__name__)
+        print json.dumps(self.twitter.search(q=query), indent=4)
 
 
-def byebye(status=0):
-    print "\nAgent %s shutting down. Bye!"%TWITTER_USERNAME
-    exit(status)
+    def finish(self, status=0):
+        print "\nAgent %s shutting down. Bye!"%TWITTER_USERNAME
+        exit(status)
 
 if __name__ == '__main__':
     main()
