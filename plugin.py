df5fe463e4997618455a13ff695263cba73fc06e
Rob McGuire-Dale <rob.mcguiredale@gmail.com>
2011-04-28 22:09:03 -0700

    Adding comments.

--- plugin.py
+++ plugin.py
@@ -3,6 +3,7 @@ The twitswarm base plugin module.
 '''
 
 import json
+from datetime import datetime
 
 class base_plugin:
     '''
@@ -35,28 +36,44 @@ class base_plugin:
                 response = twitter.search(q=listener.search_query, rpp=1)
                 listener.since_id = int(response['max_id_str'])
                 print "Obtained most current ID: %s"%listener.since_id
-           
-            # Now let's get the results from this time on
-            nr_results = 1
-            cur_page = 1
-            print "Getting current results for query ``%s``:."\
-                    %listener.search_query
-            while nr_results > 0:
-                response = twitter.search(q=listener.search_query, 
-                        since_id=listener.since_id, rpp=100, page=cur_page)
-                print "Begin page %s >>>"%cur_page
-                for result in response['results']:
-                    print '"%s" from @%s'%(result['text'], result['from_user'])
-                    results.append(result)
-                print "<<< end page %s"%cur_page
-                listener.since_id = int(response['max_id_str'])
-                print "Since ID changed to %s"%listener.since_id
-                nr_results = len(response['results'])
-                cur_page += 1
-            print "That's all for now!"
 
-            # Finally, call the handler function for the current listener for
-            # each result
-            for result in results:
-                listener.handler(result, twitter)
+            # Otherwise, we have a since tweet id. Let's run our query on all
+            # tweets since this tweet ID.
+            else:           
+                # Now let's get the results from this time on
+                nr_results = 1
+                cur_page = 1
+                print "Getting current results for query ``%s``:."\
+                        %listener.search_query
+
+                # While we have returned results, get run the query, then go
+                # go to the next page of results
+                while nr_results > 0:
+                    response = twitter.search(q=listener.search_query, 
+                            since_id=listener.since_id, rpp=100, page=cur_page)
+                    print "Begin page %s >>>"%cur_page
+                    for result in response['results']:
+                        long_text = result['text']
+                        short_text = 0
+                        if len(long_text) <=30:
+                            short_text = long_text[:30]
+                        else:
+                            short_text = long_text[:27] + '...'
+                        #dt = datetime.strptime(result['created_at'],'%a, %d %b %Y %H:%M:%S')
+                        dt = result['created_at']
+                        print '\t%s "%s" from @%s'%(dt, 
+                                short_text,
+                                result['from_user'])
+                        results.append(result)
+                    print "<<< end page %s"%cur_page
+                    listener.since_id = int(response['max_id_str'])
+                    print "Since ID changed to %s"%listener.since_id
+                    nr_results = len(response['results'])
+                    cur_page += 1
+                print "That's all for now!"
+
+                # Finally, call the handler function for the current listener
+                # for each result
+                for result in results:
+                    listener.handler(result, twitter)
 
